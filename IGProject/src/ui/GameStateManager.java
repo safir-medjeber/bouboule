23b29047dd40672c14ab36ca8c2a14e4626f2052
Safir MEDJEBER <medjeber@informatique.univ-paris-diderot.fr>
2015-04-10 10:04:19 +0200

    MainMenukeylistener + FocusListener

--- IGProject/src/ui/GameStateManager.java
+++ IGProject/src/ui/GameStateManager.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.Stack;
 
@@ -10,12 +11,27 @@ public class GameStateManager {
 
 	public static final int GAME = 0;
 	public static final int MAIN = 1;
-
+	public static final int LOAD = 2;
+	public static final int INSTRUCTIONS = 3;
+	public static final int SCORES = 4;
+	
 	private Game game;
-
 	private Stack<GameState> gameStates;
 
+	public static int HEIGHT_SCREEN;
+	public static int WIDTH_SCREEN;
+
+	public void getSizeOfScreen(){
+		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		HEIGHT_SCREEN = (int)dimension.getHeight();
+		WIDTH_SCREEN = (int)dimension.getWidth();
+	}
+
+
+
+
 	public GameStateManager(Game game) {
+		getSizeOfScreen();
 		this.game = game;
 		gameStates = new Stack<GameState>();
 		pushState(MAIN);
