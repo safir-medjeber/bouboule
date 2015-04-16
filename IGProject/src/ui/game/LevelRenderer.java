ed56b201696fe5f52d6249eaf57a4dfd50df498d
GEDIK marc <gedik@informatique.univ-paris-diderot.fr>
2015-04-16 23:18:45 +0200

    Nouveau Game Loop

--- IGProject/src/ui/game/LevelRenderer.java
+++ IGProject/src/ui/game/LevelRenderer.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -29,9 +30,13 @@ public class LevelRenderer extends GameState {
 		setDoubleBuffered(true);
 		camera.setBounds(0, level.getWidth() * tileSize, 0, level.getHeight()
 				* tileSize);
+//		background = GraphicsEnvironment.getLocalGraphicsEnvironment()
+//				.getDefaultScreenDevice().getDefaultConfiguration()
+//				.createCompatibleImage(2000, 2000, Transparency.OPAQUE);
 		background = GraphicsEnvironment.getLocalGraphicsEnvironment()
 				.getDefaultScreenDevice().getDefaultConfiguration()
 				.createCompatibleImage(Game.WIDTH, Game.HEIGHT, Transparency.OPAQUE);
+
 	}
 
 	@Override
@@ -48,7 +53,7 @@ public class LevelRenderer extends GameState {
 		float offsetY = -camera.getY() + Game.HEIGHT / 2;
 		
 		Graphics2D g2d = (Graphics2D) g;
-		
+	    
 		Graphics2D bg = (Graphics2D) background.getGraphics();
 		bg.setPaint(Color.BLACK);
 		bg.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
@@ -60,8 +65,8 @@ public class LevelRenderer extends GameState {
 		bg.setPaint(Color.RED);
 		for(GameObject gameObject : level.getGameObjects())
 			bg.draw(gameObject.getBounds());
-		
 		g2d.drawImage(background, 0, 0, null);
+		//g2d.drawImage(background.getScaledInstance(Game.WIDTH, Game.HEIGHT, Image.SCALE_SMOOTH), 0, 0, null);
 	}
 
 	@Override
